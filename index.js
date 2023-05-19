3da9c71cc39d1b9f7dbc2fef853ef24109b5f929
EloizaBAB <122025679+EloizaBAB@users.noreply.github.com>
2023-05-19 17:28:19 +0100

    created the cards and added image and embeded maps, made them responsive

---
+++ index.js
@@ -0,0 +1,5 @@
+function displayOptions() {
+  let selectedLevel = document.querySelector("#select-level");
+  let selectValue = selectedLevel.value;
+  let cardContainer = document.querySelector("#cards");
+}
